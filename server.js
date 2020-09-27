04a90d54304203196dd606ecd7623d0c05b3762f
Raul <raul.coto.diaz@gmail.com>
2020-09-26 19:43:02 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const { text } = require('express');
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    //res.send('Hola mundo')
+
+    res.render('home', {
+        nombre: 'raúl diaZZ'
+    });
+});
+
+app.get('/about', (req, res) => {
+    //res.send('Hola mundo')
+
+    res.render('about');
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando puerto ${port}`);
+})
